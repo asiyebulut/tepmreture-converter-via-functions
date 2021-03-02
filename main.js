47ed7bb4768dab46288302e540391de13426a4ee
asiyebulut <atubakesenel@gmail.com>
2021-03-02 13:56:03 -0800

    convert random numbers from F to C

---
+++ main.js
@@ -0,0 +1,4 @@
+var fahrenheitTemp = Math.random();
+console.log(fahrenheitTemp);
+var convertFToC = (fahrenheitTemp - 32) * 5 / 9;
+console.log('It is ' + fahrenheitTemp + '\u00B0' + 'F today. That is ' + convertFToC + '\u00B0' + 'C.');
